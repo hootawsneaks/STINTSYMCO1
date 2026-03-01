8cc804c3aebbaf509f51ccb90f88a50963843915
hootawsneaks <162249452+hootawsneaks@users.noreply.github.com>
2026-03-02 00:22:58 +0800

    Initial Commit

---
+++ setup.ps1
@@ -0,0 +1,25 @@
+Write-Host "May God be with ye."
+$filename = ".venv"
+$targetDir = Join-Path -Path "." -ChildPath $filename
+
+# if .venv exists
+if (Test-Path -Path $targetDir) {
+	.\.venv\Scripts\Activate.ps1
+	uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+	uv pip install ultralytics
+	Write-Host "Enter the venv with: .venv\Scripts\activate"
+} 
+else {
+	if (-not (Get-Command uv -ErrorAction SilentlyContinue) ) {
+		powershell -ExecutionPolicy ByPass -c "irm https://astral.sh/uv/install.ps1 | iex"
+		# Refresh Path
+		$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+	}
+	uv venv
+	.\.venv\Scripts\Activate.ps1
+	uv pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+	uv pip install ultralytics
+        Write-Host "Enter the venv with: .venv\Scripts\activate"
+}
+
+  
